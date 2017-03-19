3d138bf08c6f92ac138558414efa3285dfb85abf
Stephen Williams <me@alta189.com>
2017-03-19 00:55:20 -0400

    Update file

--- empowerer_manager/empowerer_manager.lua
+++ empowerer_manager/empowerer_manager.lua
@@ -1,4 +1,4 @@
 local sides = require("sides")
-local get_inventory = dofile("/opt/empowerer_manager/get_inventory")
+local get_inventory = dofile("/opt/empowerer_manager/get_inventory.lua")
 
 get_inventory(sides.top)
