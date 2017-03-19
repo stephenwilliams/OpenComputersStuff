d0619cfa3e3e86e1f6a0081c3e06fe975b3f184f
Stephen Williams <me@alta189.com>
2017-03-19 10:29:43 -0400

    Check recipes and print output

--- empowerer_manager/empowerer_manager.lua
+++ empowerer_manager/empowerer_manager.lua
@@ -4,5 +4,14 @@ local sides = require("sides")
 -- Import internal code
 dofile("/opt/empowerer_manager/init.lua")()
 local get_inventory = dofile("/opt/empowerer_manager/get_inventory.lua")
+local table_utils = dofile("/opt/empowerer_manager/table_utils.lua")
+local load_config = dofile("/opt/empowerer_manager/load_config.lua")
 
-get_inventory(sides.top)
+local config = load_config()
+
+local chestInv = get_inventory(sides.top)
+
+for recipe in config.recipes dofile
+  print("Checking recipe: "..recipe.name)
+  print(table_utils.containsTable(chestInv, recipe.items))
+end
