7bdd4843b4fc2fb1688ad6ce39420b682b32262d
Stephen Williams <me@alta189.com>
2017-03-19 12:39:29 -0400

    prepare recipe

--- empowerer_manager/bin/get_recipe.lua
+++ empowerer_manager/bin/get_recipe.lua
@@ -4,6 +4,7 @@ return function  (chestInv, recipes)
   for _,recipe in pairs(recipes) do
     print("Checking recipe: "..recipe.name)
     if table_utils.containsTable(chestInv, recipe.items) then
+      print("Found recipe: "..recipe.name)
       return recipe
     end
   end
--- empowerer_manager/empowerer_manager.lua
+++ empowerer_manager/empowerer_manager.lua
@@ -6,12 +6,17 @@ dofile("/opt/empowerer_manager/init.lua")()
 local get_inventory = dofile("/opt/empowerer_manager/get_inventory.lua")
 local table_utils = dofile("/opt/empowerer_manager/table_utils.lua")
 local load_config = dofile("/opt/empowerer_manager/load_config.lua")
+local get_recipe = dofile("/opt/empowerer_manager/get_recipe.lua")
+local prepare_recipe = dofile("/opt/empowerer_manager/prepare_recipe.lua")
 
 local config = load_config()
 
 local chestInv = get_inventory(sides.top)
 
-for _,recipe in pairs(config.recipes) do
-  print("Checking recipe: "..recipe.name)
-  print(table_utils.containsTable(chestInv, recipe.items))
+local recipe = get_recipe(chestInv, config.recipes)
+
+if recipe ~= nil then
+  prepare_recipe(sides.top, sides.bottom, recipe)
+else
+  print("Nothing found")
 end
