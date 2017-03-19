c277e46daeb487822221155c05250835c7783bf4
Stephen Williams <me@alta189.com>
2017-03-19 09:46:39 -0400

    read config

--- empowerer_manager/bin/load_config.lua
+++ empowerer_manager/bin/load_config.lua
@@ -2,6 +2,16 @@ local fs = require("filesystem")
 local json = loadfile("/usr/lib/json.lua")()
 
 return function ()
-  local file = fs.open('/etc/empowerer_manager.cfg')
-  return json:decode(file:read("a*"))
+  local contents = ""
+
+  do
+    local file = fs.open('/etc/empowerer_manager.cfg')
+    local s = file:read(2048)
+    while s do
+      contents = contents..s
+      s = file:read(2048)
+    end
+  end
+
+  return json:decode(contents)
 end
