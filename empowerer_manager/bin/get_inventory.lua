134e355d6716c5edef006a419ae09f8e1c4a1fcc
Stephen Williams <me@alta189.com>
2017-03-19 00:48:19 -0400

    print stacks

--- empowerer_manager/bin/get_inventory.lua
+++ empowerer_manager/bin/get_inventory.lua
@@ -1,3 +1,8 @@
+local inv = require("component").inventory_component
+
 return function(side)
-    return "Hello Updated World!"
+  for slot=1,inv.getInventorySize(side) do
+    local stack = inv.getStackInSlock(side, slot)
+    print(stack.name)
+  end
 end
