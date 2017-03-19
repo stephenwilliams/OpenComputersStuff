0875d069faf33f21a0b7305de400aa82c0a1163e
Stephen Williams <me@alta189.com>
2017-03-19 12:15:59 -0400

    Fix return

--- empowerer_manager/bin/get_recipe.lua
+++ empowerer_manager/bin/get_recipe.lua
@@ -2,7 +2,7 @@ return function  (chestInv, recipes)
   for _,recipe in pairs(recipes) do
     print("Checking recipe: "..recipe.name)
     if table_utils.containsTable(chestInv, recipe.items) then
-      return recipes
+      return recipe
     end
   end
   return nil
