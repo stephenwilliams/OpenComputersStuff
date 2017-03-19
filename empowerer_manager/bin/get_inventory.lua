c5caa76fc3024549bff92b0a62ec399eb91242ba
Stephen Williams <me@alta189.com>
2017-03-19 09:58:43 -0400

    Implement get_inventory

--- empowerer_manager/bin/get_inventory.lua
+++ empowerer_manager/bin/get_inventory.lua
@@ -1,8 +1,17 @@
 local inv = require("component").inventory_controller
+local table_utils = dofile("/opt/empowerer_manager/table_utils.lua")
 
 return function(side)
+  local result = {}
   for slot=1,inv.getInventorySize(side) do
     local stack = inv.getStackInSlot(side, slot)
-    print(stack.name)
+    if stack ~= nil then
+      if table_utils.contains(result, stack.label) then
+        result[stack.lab] = result[stack.lab] + stack.size
+      else
+        result[stack.lab] = stack.size
+      end
+    end
   end
+  return result
 end
