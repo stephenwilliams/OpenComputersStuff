e7372a6f60fb7eb271012dda46da71d048d0d444
Stephen Williams <me@alta189.com>
2017-03-18 23:28:24 -0400

    Fix update

--- update.lua
+++ update.lua
@@ -1,13 +1,20 @@
 local shell = require("shell")
 local fs = require("filesystem")
 
-if !fs.exists("./lib") then
-    fs.makeDirectory('./lib')
+if not fs.exists("/home/lib") then
+    fs.makeDirectory('/home/lib')
 end
 
-if !fs.exists("./bin") then
-    fs.makeDirectory('./bin')
+if not fs.exists("/home/bin") then
+    fs.makeDirectory('/home/bin')
 end
+
+if not fs.exists("/home/backup") then
+    fs.makeDirectory('/home/backup')
+end
+
+-- Backup updatge incase we break it
+shell.execute("mv ./update.lua ./backup/update.lua")
 
 shell.execute("wget -fq https://raw.githubusercontent.com/stephenwilliams/OpenComputersStuff/master/update.lua ./update.lua")
 shell.execute("wget -fq https://raw.githubusercontent.com/stephenwilliams/OpenComputersStuff/master/lib/json.lua ./lib/json.lua")
