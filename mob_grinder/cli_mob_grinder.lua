e8ab5d37616d73629452c4e648e94c105b13eab5
Stephen Williams <me@alta189.com>
2017-03-26 12:42:25 -0400

    Finish CLI

--- mob_grinder/cli_mob_grinder.lua
+++ mob_grinder/cli_mob_grinder.lua
@@ -40,15 +40,52 @@ function toggleSpawner(mob)
   end
 end
 
+function listSpawners()
+  for _,spawner in pairs(config.spawners) do
+    print(spawner.name)
+  end
+end
+
+function toggleLights()
+  if (rs.getBundledOutput(bundle, colors[config["lights"]]) > 0) then
+    print("Turned lights off")
+  else
+    rs.setBundledOutput(bundle, colors[config["lights"]], 15)
+    print("Turned lights on")
+  end
+end
+
+function toggleGrinder()
+  if (rs.getBundledOutput(bundle, colors[config["grinder"]]) > 0) then
+    print("Turned grinder off")
+  else
+    rs.setBundledOutput(bundle, colors[config["grinder"]], 15)
+    print("Turned grinder on")
+  end
+end
+
 -- END FUNCTIONS
 
 if (table.getn(args) <= 0) then
-  print("No args")
+  print("Mob Grinder CLI")
+  print("---------------")
+  print("spawn mob")
+  print("list")
+  print("grinder")
+  print("lights")
   return
 end
 
 if (args[1] == "spawn" and table.getn(args) == 2) then
   toggleSpawner(args[2])
+elseif (args[1] == "spawn" and table.getn(args) ~= 2)
+  print("Usage is spawn <mob>")
+elseif (args[1] == "list") then
+  listSpawners()
+elseif (args[1] == "grinder") then
+  toggleGrinder()
+elseif (args[1] == "lights") then
+  toggleLights()
 end
 
 
